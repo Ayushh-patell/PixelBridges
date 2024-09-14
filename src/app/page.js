d5117cf85f810593dac56f5f144236616be80c2b
Ayushh-patell <ayush.patel.code@gmail.com>
2024-09-14 18:28:34 +0530

    added a gap between botton text in first screen

--- src/app/page.js
+++ src/app/page.js
@@ -11,7 +11,7 @@ export default function Home() {
     <div className=" w-full flex-1 relative font-[family-name:var(--font-geist-sans)]">
       <main id="HomeScreen" className=" w-full h-full flex flex-col justify-between items-center gap-5 relative">
 <div className=" w-full">
-<h1 className={` ${bebas.className} lg:gap-[45px] md:gap-[35px] sm3:gap-[25px] gap-4 xl1:text-5xl lg:text-4xl md:text-2xl text-xl flex justify-center items-center lg:pb-5 sm3:pb-10 pb-5 lg:pt-20 pt-[13dvh] mix-blend-overlay`}>
+<h1 className={` ${bebas.className} lg:gap-[45px] md:gap-[35px] sm3:gap-[25px] gap-4 xl1:text-5xl lg:text-4xl md:text-2xl text-xl flex justify-center items-center lg:pb-8 sm3:pb-10 pb-5 lg:pt-20 pt-[13dvh] mix-blend-overlay`}>
   <span>P</span>
   <span>I</span>
   <span>X</span>
@@ -46,7 +46,7 @@ export default function Home() {
       <div className=" absolute bottom-0 left-0 w-full flex sm1:flex-row flex-col-reverse justify-between items-center md:gap-16 sm1:gap-2 gap-4">
       {/* LEFT BACKGORUND COLOR */}
       <div className=" sm1:w-3/4 w-screen h-28 bg-white mix-blend-overlay sm1:-translate-x-10 -translate-x-[1.7rem] absolute z-[-5] sm1:top-1/2 sm1:bottom-auto bottom-0 sm1:-translate-y-1/2 left-0"/>
-        <div className={` sm1:w-3/4 w-full flex-shrink-0 h-28 text-white lg:text-base md:text-xs sm4:text-[10px] text-[8px] sm1:-translate-x-10 sm1:pl-10 flex sm1:flex-row flex-col sm1:justify-between justify-around items-center ${mont.className}`}>
+        <div className={` sm1:w-3/4 w-full flex-shrink-0 h-28 text-white lg:text-base md:text-xs sm4:text-[10px] text-[8px] sm1:-translate-x-10 sm1:pl-10 flex sm1:flex-row flex-col sm1:justify-between justify-around items-center lg:gap-10 md:gap-5 gap-2 ${mont.className}`}>
           <p className=' font-bold flex flex-col justify-center sm1:items-start items-center sm1:text-left text-center'>
           <span className=" lg:text-base md:text-xs text-[10px]">At Pixelbridges, we turn your website into a powerful tool</span> 
           <span className=" font-extralight">that looks great and performs even better, be it revamping or creating from scratch.</span>
