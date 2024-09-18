09675c5ef747da4d88f542581e0eff9630847ea6
Ayushh-patell <ayush.patel.code@gmail.com>
2024-09-18 11:19:45 +0530

    new expertise transition animation

--- src/app/Components/ExpertiseScreen/ExpertiseScreen.jsx
+++ src/app/Components/ExpertiseScreen/ExpertiseScreen.jsx
@@ -7,15 +7,17 @@ import RightCarousel from './RightCarousel'
 import gsap from 'gsap';
 import Image from 'next/image';
 import arrow from "../../PixelBridges/arrow.png"
+import { CustomEase } from 'gsap/all';
 
 const leftCarouselTexts = ["New Name and Identity", "Modern Themes & Visuals", "Enhanced User Experience", "Fully Responsive & Mobile Friendly"]
 const rightCarouselTexts = ["Custom Design & Coding", "E-commerce Integration", "SEO Optimized", "Scalable & Secure"]
 
 const ExpertiseScreen = () => {
 
-  const [isLeftOn, setisLeftOn] = useState(true)
-  const [leftCurrSlide, setleftCurrSlide] = useState(0)
-  const [rightCurrSlide, setrightCurrSlide] = useState(0)
+  const [isLeftOn, setisLeftOn] = useState(true);
+  const [leftCurrSlide, setleftCurrSlide] = useState(0);
+  const [rightCurrSlide, setrightCurrSlide] = useState(0);
+  gsap.registerPlugin(CustomEase)
 
   // ANIMATION FOR LEFT RIGHT CAROUSEL POSITION
   useEffect(() => {
@@ -33,12 +35,38 @@ const ExpertiseScreen = () => {
     }
     else {
       if(isLeftOn) {
-        gsap.to("#leftCarousel", {left:0, opacity:1, duration:1, ease:'none'})
-        gsap.to("#rightCarousel", {right:"-100%" ,opacity:0, duration:1, ease:'none'})
+        gsap.set("#leftCarousel", {pointerEvents:"all"})
+        gsap.set("#rightCarousel", {pointerEvents:"none"})
+
+        
+        gsap.to("#rightCarousel", {opacity:0, duration:0.7, ease:'none'})
+        gsap.to("#centerTextLeftHeading", { translateY:"100%", opacity:0, duration:0.2, ease:'power1.in'})
+        gsap.to("#centerTextLeftHeading", { scale:0.8, duration:0.2, ease:'power1.in'})
+        gsap.to("#centerTextLeftPara", { translateY:"-60%", opacity:0, duration:0.2, ease:'power1.in'})
+        gsap.to("#centerTextLeftPara", {scale:0.9, duration:0.2, ease:'power1.in'})
+        gsap.to("#centerTextLeftBox", { scale:0.3, duration:0.2, ease:'power1.in'})
+
+        gsap.to("#leftCarousel", { opacity:1, duration:0.6, ease:'none', delay:0.3})
+        gsap.to("#centerTextRightHeading", { scale:1, translateY:0, opacity:1, duration:0.7, ease:CustomEase.create("custom", "M0,0 C0.194,0 0.525,1.097 0.675,1.098 0.856,1.098 0.94,1.018 1,1 "), delay:0.3})
+        gsap.to("#centerTextRightPara", { translateY:0, scale:1, opacity:1, duration:0.7, ease:CustomEase.create("custom", "M0,0 C0.194,0 0.525,1.097 0.675,1.098 0.856,1.098 0.94,1.018 1,1 "), delay:0.3})
+        gsap.to("#centerTextRightBox", { scale:1, duration:0.7, ease:CustomEase.create("custom", "M0,0 C0.194,0 0.525,1.097 0.675,1.098 0.856,1.098 0.94,1.018 1,1 "), delay:0.4})
       }
       else {
-        gsap.to("#leftCarousel", {left:"-100%", opacity:0, duration:1, ease:'none'})
-        gsap.to("#rightCarousel", {right:0, opacity:1, duration:1, ease:'none'})
+        gsap.set("#leftCarousel", {pointerEvents:"none"})
+        gsap.set("#rightCarousel", {pointerEvents:"all"})
+
+
+        gsap.to("#leftCarousel", { opacity:0, duration:0.7, ease:'none'})
+        gsap.to("#centerTextRightHeading", { translateY:"100%", opacity:0, duration:0.2, ease:'power1.in'})
+        gsap.to("#centerTextRightHeading", { scale:0.8, duration:0.2, ease:'power1.in'})
+        gsap.to("#centerTextRightPara", { translateY:"-60%", opacity:0, duration:0.2, ease:'power1.in'})
+        gsap.to("#centerTextRightPara", {scale:0.9, duration:0.2, ease:'power1.in'})
+        gsap.to("#centerTextRightBox", { scale:0.3, duration:0.2, ease:'power1.in'})
+
+        gsap.to("#rightCarousel", { opacity:1, duration:0.6, ease:'none', delay:0.3})
+        gsap.to("#centerTextLeftHeading", { scale:1, translateY:0, opacity:1, duration:0.7, ease:CustomEase.create("custom", "M0,0 C0.194,0 0.525,1.097 0.675,1.098 0.856,1.098 0.94,1.018 1,1 "), delay:0.3})
+        gsap.to("#centerTextLeftPara", { translateY:0, scale:1, opacity:1, duration:0.7, ease:CustomEase.create("custom", "M0,0 C0.194,0 0.525,1.097 0.675,1.098 0.856,1.098 0.94,1.018 1,1 "), delay:0.3})
+        gsap.to("#centerTextLeftBox", { scale:1, duration:0.7, ease:CustomEase.create("custom", "M0,0 C0.194,0 0.525,1.097 0.675,1.098 0.856,1.098 0.94,1.018 1,1 "), delay:0.4})
       }
     }
     
@@ -164,19 +192,19 @@ const ExpertiseScreen = () => {
 <div id='centerTextRight' className={` ${isLeftOn? " opacity-100 pointer-events-auto":" md:opacity-0 opacity-100 pointer-events-none"} transition-opacity duration-700 flex flex-col justify-between items-center w-full h-full p-10 md:pt-40 pt-[40dvh] xl1:pl-44 md:pl-32 md:bg-[#110E17F7] bg-[#110E17] `}>
 
 <div>
-<button onClick={() => setisLeftOn((prev) => !prev)} className={` md:h-[14px] h-2 lg1:w-1/2 md:w-2/3 w-4/5 rounded-full bg-[#d9d9d9]/10 flex justify-start items-center mb-12 mx-auto`}>
-          <div className={` w-1/2 rounded-full h-full ${isLeftOn?" bg-[#B896FF] translate-x-0":" bg-[#5E81FF] md:translate-x-32 translate-x-12"} transition-all duration-700`}></div>
+        <button onClick={() => setisLeftOn((prev) => !prev)} className={` md:h-[14px] h-2 lg1:w-1/2 md:w-2/3 w-4/5 rounded-full bg-[#d9d9d9]/10 flex justify-start items-center mb-12 mx-auto`}>
+          <div className={`  rounded-full h-full ${isLeftOn?" bg-[#B896FF] translate-x-0 w-1/2 opacity-100":" bg-[#5E81FF] md:translate-x-32 translate-x-[150%] opacity-0 w-1/4"} transition-all duration-500 ease-in-out`}></div>
         </button>
 
-        <div className=' relative text-center p-3'>
+        <div id='centerTextRightHeading' className=' relative text-center p-3'>
             <p className={`${bebas.className} xl1:text-7xl lg1:text-6xl sm:text-4xl text-3xl relative z-10`}>GIVE YOUR WEBSITE A</p>
             <p className={`${bebas.className} xl1:text-7xl lg1:text-6xl sm:text-4xl text-3xl relative z-10`}>FRESH START</p>
 
-            <div aria-hidden className=' bg-mainBg h-full w-1/2 absolute top-0 right-0 z-0'/>
+            <div id='centerTextRightBox' aria-hidden className=' origin-bottom-left bg-mainBg h-full w-1/2 absolute top-0 right-0 z-0'/>
         </div>
 </div>
 
-        <div className=' text-center xl1:text-xl lg1:text-lg text-sm space-y-3 px bg-[#110E17]-4'>
+        <div id='centerTextRightPara' className=' text-center xl1:text-xl lg1:text-lg text-sm space-y-3 px bg-[#110E17]-4'>
             <p>
             We specialize in breathing new life into outdated websites by giving them a <span className=' text-[#AD86FF] font-bold xl1:text-xl lg1:text-lg text-base'>fresh identity,  modern themes,  and an enhanced user experience.</span> 
             </p>
@@ -193,19 +221,19 @@ const ExpertiseScreen = () => {
 <div id='centerTextLeft' className={` ${!isLeftOn? " opacity-100 pointer-events-auto":" opacity-0 pointer-events-none"} transition-opacity duration-700 absolute top-0 left-0 flex flex-col justify-between items-center  w-full h-full p-10 md:pt-40 pt-[40dvh] xl1:pr-44 md:pr-32 md:bg-[#110E17F7] bg-[#110E17] `}>
 
 <div>
-<button onClick={() => setisLeftOn((prev) => !prev)} className={` md:h-[14px] h-2 lg1:w-1/2 md:w-2/3 w-4/5 rounded-full bg-[#d9d9d9]/10 flex justify-end items-center mx-auto mb-10`}>
-        <div className={` w-1/2 rounded-full h-full ${!isLeftOn?" bg-[#5E81FF] translate-x-0":" bg-mainBg md:-translate-x-32 -translate-x-12"} transition-all duration-700`}></div>
+        <button onClick={() => setisLeftOn((prev) => !prev)} className={` md:h-[14px] h-2 lg1:w-1/2 md:w-2/3 w-4/5 rounded-full bg-[#d9d9d9]/10 flex justify-end items-center mx-auto mb-10`}>
+          <div className={`  rounded-full h-full ${!isLeftOn?" bg-[#5E81FF] translate-x-0 w-1/2 opacity-100":" bg-mainBg md:-translate-x-32 -translate-x-[150%] opacity-0 w-1/4"} transition-all duration-500 ease-in-out`}></div>
         </button>
 
-        <div className=' relative text-center p-3'>
+        <div id='centerTextLeftHeading' className=' relative text-center p-3'>
             <p className={`${bebas.className} xl1:text-7xl lg1:text-6xl sm:text-4xl text-3xl relative z-10`}>BUILD FROM SCRATCH,</p>
             <p className={`${bebas.className} xl1:text-7xl lg1:text-6xl sm:text-4xl text-3xl relative z-10`}>TAILORED TO YOUR NEEDS</p>
 
-            <div aria-hidden className=' bg-[#5E81FF] h-full w-1/2 absolute top-0 right-0 z-0'/>
+            <div id='centerTextLeftBox' aria-hidden className=' origin-bottom-left bg-[#5E81FF] h-full w-1/2 absolute top-0 right-0 z-0'/>
         </div>
 </div>
 
-        <div className=' text-center xl1:text-xl lg1:text-lg text-sm space-y-3 px bg-[#110E17]-4'>
+        <div id='centerTextLeftPara' className=' text-center xl1:text-xl lg1:text-lg text-sm space-y-3 px bg-[#110E17]-4'>
             <p>
             Need a new website built from scratch? Our expert designers and developers create bespoke websites that are tailor-made for your business. 
             </p>
